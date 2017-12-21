128be81717b0af30ff8544d6a223eb0d934bfdeb
yangmeeso <yangmeeso@gmail.com>
2017-12-20 18:33:10 -0800

    more updates

--- letter.js
+++ letter.js
@@ -1,13 +1,19 @@
 // Letter: Used for each letter in the current word. Each letter object should either display an underlying character, or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. This should contain letter specific logic and data.
 
 var wordsArray = ["oregon", "homework", "javascript"];
-var word = "";
+var words = "";
 var blanks = 0;
+var chosenWords = [];
 var underscores = [];
 
 function game() {
-    word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
-    underscores = word.split("");
+    words = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    chosenWords = words.split("");
+    blanks = chosenWords.length;
+
+    underscores = [];
+    for (var i = 0; i < blanks;) {
+        underscores.push("_");
+    }
     
-    blanks = underscores.length;
-}
+};
